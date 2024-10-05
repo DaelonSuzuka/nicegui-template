12be7e575b3fee577bc44d72683b2d1da7a7bc8e
David Kincaid <daelonsuzuka@gmail.com>
2024-10-05 15:06:30 -0400

    Remove command palette

--- src/main.py
+++ src/main.py
@@ -1,21 +1,12 @@
 from nicegui import ui
-from nicegui.events import KeyEventArguments
-from command_palette import CommandPalette
 
 
 @ui.page('/')
 def index():
-    async def handle_key(e: KeyEventArguments):
-        if e.modifiers.shift and e.modifiers.ctrl and e.action.keydown and e.key == 'P':
-            options = ['one', 'two']
-            if result := await CommandPalette(options):
-                ui.notify(result, position='bottom-right')
-
-    ui.keyboard(on_key=handle_key)
+    ui.label('Hello!')
 
 
 ui.run(
     title='NiceGUI Template',
-    uvicorn_reload_includes='*.vue',
     dark=True,
 )
